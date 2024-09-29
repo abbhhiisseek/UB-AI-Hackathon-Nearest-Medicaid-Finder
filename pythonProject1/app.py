b24cb1899c7e32929901f3218019564feca29442
Abhisek Banerjee <abbhhiisseek@gmail.com>
2024-09-29 10:23:30 -0400

    Add config files for GCP deployment

--- pythonProject1/app.py
+++ pythonProject1/app.py
@@ -258,3 +258,8 @@ async def calculate_distance(request: Request):
 @app.get("/", response_class=FileResponse)
 async def root():
     return FileResponse("doctor_finder/static/index.html")
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT isn't set
+    uvicorn.run(app, host="0.0.0.0", port=port)
