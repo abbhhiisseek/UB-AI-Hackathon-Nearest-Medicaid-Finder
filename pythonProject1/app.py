90863b0c56d09ff02043b70920895aefb758cb8a
Abhisek Banerjee <abbhhiisseek@gmail.com>
2024-09-29 03:10:32 -0400

    change api to health.data.ny.gov

--- pythonProject1/app.py
+++ pythonProject1/app.py
@@ -1,4 +1,3 @@
-import logging
 import os
 from urllib.parse import urlencode
 
@@ -7,7 +6,6 @@ from pydantic import BaseModel, ValidationError, Field
 from langchain.prompts import PromptTemplate
 import requests
 import json
-import re
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, FileResponse
@@ -21,12 +19,6 @@ app.add_middleware(
     allow_methods=["*"],  # Allow all methods
     allow_headers=["*"],  # Allow all headers
 )
-
-DEPARTMENT_MAPPING = {
-    "Dentistry": "Dentist",
-    "Cardiology": "Cardio",
-    # Add more mappings as necessary
-}
 
 # 1. Get the directory of the current file (app.py)
 current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -47,23 +39,104 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 class DoctorResponse(BaseModel):
-    department: str = Field(alias="Department")
-    urgent_case: bool = Field(alias="Urgent Case")
+    service: str = Field(alias="Service")
+    specialty: str = Field(alias="Specialty")
     city: Optional[str] = Field(alias="City", default=None)  # Accepts None
-    doc_name: Optional[str] = Field(alias="Doctor Name", default=None)  # Accepts None
+    provider_name: Optional[str] = Field(alias="Provider Name", default=None)  # Accepts None
 
 
 # LangChain prompt template to ask the LLM to process symptoms and return structured data
 template = """
 You are a medical assistant. A user has provided the following symptoms:
-{symptoms}
-You need to extract and return the following information:
-1. Department: Based on the symptoms, which medical department should the patient visit?
-2. Urgent Case: Is it urgent? Answer with Yes or No.
-3. City: If the user mentioned a city, extract it. If the user mentioned a locality, guess the city and extract it.
-4. Doctor Name: If the user mentioned a doctor's name, extract it.
 
-Please return this information as a JSON object.
+{symptoms}
+
+The possible services are:
+[
+"MARRIAGE & FAMILY THERAPIST",
+"BRIDGES TO HEALTH WAIVER",
+"AUDIOLOGIST/HEARING AID",
+"CLINICAL PSYCHOLOGIST",
+"WAIVER SERVICES",
+"MENTAL HEALTH COUNSELORS",
+"OPTOMETRIST",
+"NURSE MIDWIFE",
+"HOSPITAL - INPATIENT",
+"SPEECH LANGUAGE PATHOLOGIST",
+"LONG TERM HOME HEALTH CARE",
+"CERTIFIED HOME HEALTH AGENCY",
+"LABORATORY DIRECTOR",
+"PERSONAL CARE SERVICES",
+"ADULT DAY HEALTH CARE",
+"LONG TERM CARE - ORDERED AMB (NO LAB)",
+"NURSING HOME",
+"SOCIAL CARE NETWORK SERVICE",
+"PHYSICIAN GROUP PRACTICE",
+"NURSE PRACTITIONER",
+"OCCUPATIONAL THERAPIST",
+"RESIDENTIAL TREATMENT FACILITY",
+"NURSING SERVICES",
+"OUTPATIENT CLINIC",
+"COMMUNITY SUPPORT (OPWDD)",
+"CERTIFIED DIABETES EDUCATOR",
+"ASSISTED LIVING PROGRAM",
+"CHILD (FOSTER) CARE AGENCIES",
+"SUPERVISING PHARMACIST",
+"REGISTERED NURSE",
+"DOULA (PERINATAL)",
+"AMBULANCE",
+"PODIATRIST",
+"MENTAL HEALTH REHABILITATION",
+"OPTICIAN",
+"PERSONAL EMERGENCY RESPONSE SERVICE",
+"LABORATORY",
+"HOSPITAL PHARMACY",
+"HOSPICE PROVIDERS",
+"NON-MEDICAL TRANSPORTATION",
+"LABORATORY CLINIC BASED",
+"PHYSICAL THERAPIST",
+"OPTICAL ESTABLISHMENT",
+"MULTI TYPE GROUP PRACTICE",
+"INTERMEDIATE CARE FACILITY (OPWDD)",
+"TO BE DETERMINED",
+"NATIONAL DIABETES PREVENTION PROGRAM",
+"MEDICAL EQUIPMENT SUPPLIERS & DEALER",
+"OUTPATIENT",
+"PHARMACY",
+"OPWDD STATE-OPERATED CLINIC",
+"CLINIC PHARMACY",
+"LABORATORY HOSPITAL BASED",
+"OXYGEN AND RELATED EQUIPMENT DEALER",
+"CLINICAL SOCIAL WORKER",
+"CASE MANAGEMENT SERVICES",
+"SERVICE BUREAU",
+"DENTISTS",
+"PHYSICIAN ASSISTANT",
+"HOME HEALTH CARE",
+"HEARING AID",
+"CHIROPRACTIC SERVICES",
+"DENTAL GROUP PRACTICE",
+"EARLY INTERVENTION OR SCHOOL SUPPORTIVE",
+"DIETITIANS / NUTRITIONISTS",
+"LICENSED BEHAVIOR ANALYST",
+"PHYSICIAN",
+"AUDIOLOGIST",
+"EYE PROSTHESIS FITTER",
+"LICENSED PRACTICAL NURSE",
+"CERTIFIED ASTHMA EDUCATOR"
+]
+
+The possible specialties are:
+[PLASTIC SURGERY, DENTAL PUBLIC HEALTH, SPEECH THERAPY, FORENSIC PATHOLOGY, MATERNAL & FETAL MEDICINE, DERMATOPATHOLOGY, PUBLIC HEALTH, GEN PREVENTIVE MEDICINE, METHADONE TREATMENT, OSTEOPATHIC MANIPUL MEDS, FAMILY PRACTICE, NEONATAL PERINATAL MEDS, OBSTETRICS & GYNECOLOGY, RHEUMATOLOGY, EMERGENCY MEDICINE, ORAL SURGEON, PSYCHIATRY (NOT CHILD), ORTHOPEDIC SURGERY, GENERAL SURGERY, GASTROENTEROLOGY, CHILD NEUROLOGY, MEDICALLY FRAGILE CHILDREN AND ADULTS, PRIMARY CARE, ORAL SURGERY, HEMODIALYSIS, ENDODONTIST, INFECTIOUS DISEASES, UROLOGY, PEDIATRICS, PEDIATRIC NEPHROLOGY, PROSTHODONTIST, NEUROLOGY (NOT CHILD), CHEMICAL DEPENDENCY TRMT, CHEMICAL DEPENDENCY REHAB, CERTIFIED DIABETES EDUCATOR, PHYSICAL THERAPY, NEUROLOGICAL SURGERY, MENTAL HLTH SVCS ADULT, PEDIATRIC CARDIOLOGY, MEDICAL MICROBIOLOGY, ONCOLOGY THERAPY, DERMATOLOGY, NUCLEAR MEDICINE, PEDIATRIC PULMONOLOGY, GENERAL DENTISTRY, GYNECOLOGIC ONCOLOGY, AMBULATORY SURGERY, CHEMICAL PATHOLOGY, HEMATOLOGY, CLINICAL PATHOLOGY, NATIONAL DIABETES PREVENTION PROGRAM, ANATOMIC PATHOLOGY, PHYSICAL MEDICINE & REHAB, DIAGNOSTIC RADIOLOGY, ALLERGY & IMMUNOLOGY, MENTAL HLTH SVCS CHILD, COLON & RECTAL SURGERY, PEDIATRIC ENDOCRINOLOGY, CARDIOVASCULAR DISEASE, AIDS DAY HEALTH CARE SERVICES, REPRODUCTIVE ENDOCRINE, ANESTHESIOLOGY, ORTHODONTURE, ENDOCRINOLOGY, CHILD PSYCHIATRY, PULMONARY DISEASES, OCCUPATIONAL THERAPY, ONCOLOGY, FISCAL INTERMEDIARY CDPC, DIAGNOSTIC ROENTGENOLOGY, OCCUPATIONAL MEDICINE, DENTAL CONSCIOUS SEDATION, NEUROPATHOLOGY, PEDIATRIC HEMA/ONCOLOGY, PEDIATRIC SURGERY, DENTAL ANESTHESIOLOGIST, OPHTHALMOLOGY, SCHOOL SUPP HLTH SVC PROG, PSYCHIATRY & NEUROLOGY, MEDICAL GENETICS, PEDIATRIC CRITICAL CARE, GASTROENTOLOGY, PERIODONTIST, MENTAL HLTH SVCS, NEPHROLOGY, THERAPEUTIC RADIOLOGY, CERTIFIED ASTHMA EDUCATOR, EARLY INTERVENTION, PEDODONTIST, THORACIC SURGERY, ANATOMIC & CLINICAL PATH, INTERNAL MEDICINE, OTOLARYNGOLOGY]
+
+You need to extract and return the following information:
+1. Service: Based on the symptoms, which area of service does the patient need?
+2. Specialty: Will the patient need a specialist to address their symptoms? If so which Specialty?
+3. City: If the user mentioned a city, extract it. If the user mentioned a locality, guess the city and extract it. Otherwise default to Buffalo.
+4. Provider Name: If the user mentioned a facility or Doctor's name, extract it.
+5. Medicaid Coverage: (Select from FFS MCO of OPRA only if specified)
+
+Please return ONLY this information as a JSON object.
 """
 
 # Initialize LangChain prompt template
@@ -108,59 +181,53 @@ async def process_data(request: Request):
     formatted_prompt = prompt_template.format(symptoms=symptoms)
 
     # Call the Google Gemini API
-    api_response = call_gemini_api(formatted_prompt)
+    ai_response = call_gemini_api(formatted_prompt)
+    print(ai_response)
 
     # Extracting 'text' from the response
-    candidates = api_response.get('candidates', [])
+    candidates = ai_response.get('candidates', [])
     if candidates and 'content' in candidates[0] and 'parts' in candidates[0]['content']:
         generated_text = candidates[0]['content']['parts'][0].get('text', '')
     else:
         return {"error": "Invalid response structure from API."}
 
-    # Strip the backticks and extract valid JSON content using regex
+    # Strip the backticks and extract valid JSON content
     try:
-        # Use regex to extract the JSON part only (everything between the ```json and ``` markers)
-        json_match = re.search(r'```json(.*?)```', generated_text, re.DOTALL)
-        if not json_match:
-            return {"error": "No valid JSON found in the response."}
+        # Remove the backticks and any non-JSON characters
+        generated_text = generated_text.replace("```json", "").replace("```", "").strip()
 
-        # Extract and clean up the JSON content
-        json_content = json_match.group(1).strip()
-
+        # print(generated_text)
         # Parse the cleaned JSON
-        response_data = json.loads(json_content)
+        generated_data = json.loads(generated_text)
 
         # Use Pydantic to validate and return structured data
-        doctor_response = DoctorResponse(**response_data)
+        doctor_response = DoctorResponse(**generated_data)
 
-        # Extract necessary parameters for NPI Registry API
-        department = doctor_response.department
-
-        if department in DEPARTMENT_MAPPING:
-            department = DEPARTMENT_MAPPING[department]
-
-        city = doctor_response.city
-        first_name = doctor_response.doc_name
-
-        # Prepare the NPI Registry API call
+        # Prepare the Registry API call
         params = {
-            "taxonomy_description": department.lower() if department else "",
-            "city": city if city else "",
-            "first_name": first_name if first_name else "",
-            "version": "2.1"
+            "$$app_token" : "WejkcY9YZD6Rl4kDF4br66uXx",
+            # "profession_or_service": doctor_response.service.upper() if doctor_response.service else "",
+            "provider_specialty": doctor_response.specialty.upper() if doctor_response.specialty else "",
+            "city": doctor_response.city.upper() if doctor_response.city else "",
+            "state": "NY",
+            "mmis_name": doctor_response.provider_name.upper() if doctor_response.provider_name else "",
         }
         filtered_params = {k: v for k, v in params.items() if v}
-        npi_registry_url = "https://npiregistry.cms.hhs.gov/api/?" + urlencode(filtered_params)
-        logging.info("npi_registry_url",npi_registry_url)
-        npi_response = requests.get(npi_registry_url)
+        provider_registry_url = "https://health.data.ny.gov/resource/keti-qx5t.json?" + urlencode(filtered_params)
+        if doctor_response.service: # need to do this bc many are just in as PHYSICIAN
+            provider_registry_url += f"&$where=profession_or_service in('PHYSICIAN','{doctor_response.service.upper()}')"
+        registry_response = requests.get(provider_registry_url)
 
-        # Append NPI Registry data to the existing response
+        print(filtered_params)
+        print(len(registry_response.content))
+
+        # Append Registry data to the existing response
         response = {
-            "department": department,
-            "urgent_case": doctor_response.urgent_case,
-            "city": city,
-            "doc_name": first_name,
-            "npi_registry_data": npi_response.json() if npi_response.status_code == 200 else None
+            "service": doctor_response.service,
+            "specialty": doctor_response.specialty,
+            "city": doctor_response.city,
+            "provider_name": doctor_response.provider_name,
+            "registry_data": registry_response.json() if registry_response.status_code == 200 else None
         }
 
         return response
